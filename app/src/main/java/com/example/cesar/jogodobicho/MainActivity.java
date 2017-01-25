9594ab9eb743b30ff06d4a763f52f7aba98327bb
César Augusto <cesaraugustokg6@hotmail.com>
2017-01-24 23:09:26 -0200

    Dialog Fragment

--- app/src/main/java/com/example/cesar/jogodobicho/MainActivity.java
+++ app/src/main/java/com/example/cesar/jogodobicho/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cesar.jogodobicho;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        TextView txt = (TextView) findViewById(R.id.txt);
+        txt.setBackgroundResource(R.drawable.circle_text);
+    }
+
+    public void definirTitulares(View view) {
+        FragmentManager fm = getSupportFragmentManager();
+        PegaTitulares pegaTitulares = PegaTitulares.newInstance("Some Title");
+        pegaTitulares.show(fm, "fragment_edit_name");
+
     }
 }
---
+++ app/src/main/java/com/example/cesar/jogodobicho/PegaTitulares.java
@@ -0,0 +1,55 @@
+package com.example.cesar.jogodobicho;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PegaTitulares extends DialogFragment {
+
+    private EditText mEditText;
+
+    public PegaTitulares() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static PegaTitulares newInstance(String title) {
+        PegaTitulares frag = new PegaTitulares();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pega_titulares, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        // Fetch arguments from bundle and set title
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+        // Show soft keyboard automatically and request focus to field
+        mEditText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+}
